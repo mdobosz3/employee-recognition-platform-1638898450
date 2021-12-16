779fc0efc29a43a85b8a020952034cddda25ebfe
Michał Dobosz <doboszmichal@o2.pl>
2021-12-16 22:53:11 +0100

    complete task1

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+employee_1=Employee.create(email: "jedynka@gmail.com", password: "111111")
+employee_2=Employee.create(email: "dwojka@gmail.com", password: "222222")
+employee_3=Employee.create(email: "trojka@gmail.com", password: "333333")
+employee_4=Employee.create(email: "czworka@gmail.com", password: "444444")
+employee_5=Employee.create(email: "piatka@gmail.com", password: "555555")
