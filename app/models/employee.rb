afaf17f11778b9227a6d38d9b1a52a10ed6574cd
Michał Dobosz <doboszmichal@o2.pl>
2022-01-06 21:35:03 +0100

    Add new root and schema kudo

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -5,4 +5,7 @@ class Employee < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :given_kudo, class_name: "Kudo", foreign_key: 'giver_id', dependent: :destroy, inverse_of: :kudo
+  has_many :receiven_kudo, class_name: "Kudo", foreign_key: 'receiver_id', dependent: :destroy, inverse_of: :kudo
 end
