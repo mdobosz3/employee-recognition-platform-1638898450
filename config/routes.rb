531bfbd146a1d39ffb3f8d297c87cd2adc806963
Michał Dobosz <doboszmichal@o2.pl>
2022-01-22 16:28:37 +0100

    add routes to admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  devise_for :admin_users, path: 'admin_users'
+  devise_for :admin_users, path: 'admin'
   devise_for :employees, path: 'employees'
   resources :kudos
+
   root 'kudos#index'
+
+  namespace :admin do
+    root to: 'pages#dashboard'
+  end
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
