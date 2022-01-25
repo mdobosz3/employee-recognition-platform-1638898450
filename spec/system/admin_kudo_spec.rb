e13e0546383028f4131514534bb6e8934f16d4b5
Michał Dobosz <doboszmichal@o2.pl>
2022-01-26 00:23:06 +0100

    admin kudo CRUD spec

---
+++ spec/system/admin_kudo_spec.rb
@@ -0,0 +1,48 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Kudo check', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  let!(:employee1) { create(:employee) }
+  let!(:employee2) { create(:employee) }
+  let!(:admin_user) { create(:admin_user) }
+  let!(:kudo) { build(:kudo) }
+
+  it 'crud kudo' do
+    visit root_path
+    click_link 'Admin Log In'
+    fill_in 'Email', with: admin_user.email
+    fill_in 'Password', with: admin_user.password
+    click_button 'Log in'
+    expect(page).to have_content 'Signed in successfully.'
+    expect(page).to have_content 'Admin Dashboard'
+
+    click_link 'Kudos index'
+    click_link 'New Kudo'
+    fill_in 'Title', with: kudo.title
+    fill_in 'Content', with: kudo.content
+    select employee1.email, from: 'kudo[giver_id]'
+    select employee2.email, from: 'kudo[receiver_id]'
+    click_button 'Create Kudo'
+    expect(page).to have_content 'Kudo was successfully created.'
+    expect(page).to have_content 'title kudo'
+
+    click_link 'Edit'
+    fill_in 'Title', with: 'title test edit'
+    fill_in 'Content', with: 'content test edit'
+    select employee2.email, from: 'kudo[giver_id]'
+    select employee1.email, from: 'kudo[receiver_id]'
+    click_button 'Update Kudo'
+    expect(page).to have_content 'Kudo was successfully updated.'
+    expect(page).to have_content 'title test edit'
+    click_link 'Back'
+
+    click_link 'Delete'
+    expect(page).to have_content 'Kudo was successfully destroyed.'
+    expect(page).to have_content 'All Kudos'
+  end
+end
