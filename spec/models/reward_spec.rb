099f295810244387ab4357d93a52708b501747b3
Michał Dobosz <doboszmichal@o2.pl>
2022-02-10 16:16:23 +0100

    fix rubocop

--- spec/models/reward_spec.rb
+++ spec/models/reward_spec.rb
@@ -3,24 +3,24 @@
 require 'rails_helper'
 
 RSpec.describe Reward, type: :model do
-    let!(:reward) { create(:reward) }
+  let!(:reward) { create(:reward) }
 
-    it "is valid with valid attributes" do
-        expect(reward).to be_valid
-    end
+  it 'is valid with valid attributes' do
+    expect(reward).to be_valid
+  end
 
-    it "is not valid without a title" do
-        reward.title = nil
-        expect(reward).to_not be_valid
-    end
+  it 'is not valid without a title' do
+    reward.title = nil
+    expect(reward).not_to be_valid
+  end
 
-    it "is not valid without a description" do
-        reward.description = nil
-        expect(reward).to_not be_valid
-    end
+  it 'is not valid without a description' do
+    reward.description = nil
+    expect(reward).not_to be_valid
+  end
 
-    it "is not valid without a price" do
-        reward.price = nil
-        expect(reward).to_not be_valid
-    end
+  it 'is not valid without a price' do
+    reward.price = nil
+    expect(reward).not_to be_valid
+  end
 end
