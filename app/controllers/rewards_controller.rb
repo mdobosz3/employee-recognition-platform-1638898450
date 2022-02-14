1c788c2321c54e06d691eeec9b203a0deb69d6ac
Michał Dobosz <doboszmichal@o2.pl>
2022-02-14 22:58:19 +0100

    add rewards controller

---
+++ app/controllers/rewards_controller.rb
@@ -0,0 +1,51 @@
+# frozen_string_literal: true
+
+class RewardsController < ApplicationController
+  def index
+    render :index, locals: { rewards: Reward.all }
+  end
+
+  def show
+    render :show, locals: { reward: reward_find }
+  end
+
+    #def new
+    #  render :new, locals: { reward: Reward.new }
+    #end
+
+    #def edit
+    #  render :edit, locals: { reward: reward_find }
+    #end
+
+    #def create
+    #  reward = Reward.new(reward_params)
+    #  if reward.save
+    #    redirect_to admin_rewards_path(reward), notice: 'Reward was successfully created.'
+    #  else
+    #    render :new, locals: { reward: reward }
+    #  end
+    #end
+
+    #def update
+    #  if reward_find.update(reward_params)
+    #    redirect_to admin_rewards_path(reward_find), notice: 'Reward was successfully updated.'
+    #  else
+    #    render :edit, locals: { reward: reward_find }
+    #  end
+    #end
+
+    #def destroy
+    #  reward_find.destroy
+    #  redirect_to admin_rewards_url, notice: 'Reward was successfully destroyed.'
+    #end
+
+  private
+
+  def reward_find
+    @reward_find ||= Reward.find(params[:id])
+  end
+
+  def reward_params
+    params.require(:reward).permit(:title, :description, :price)
+  end
+end
