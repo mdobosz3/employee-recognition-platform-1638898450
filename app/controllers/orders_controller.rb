556c67a30b8a5559450cc0b69ae228e840fe012b
Michał Dobosz <doboszmichal@o2.pl>
2022-03-07 09:48:12 +0100

    add method index to orders controller

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,20 +2,24 @@
 
 class OrdersController < ApplicationController
   before_action :authenticate_employee!
+  def index
+    render :index, locals: { orders: Order.all }
+  end
 
   def create
     if current_employee.kudo_points < reward.price
       redirect_to rewards_path, notice: "You don't have enough kudos."
     else
-      order = Order.new(employee: current_employee, reward: reward)
+      order = Order.new(employee: current_employee, reward: reward, reward_snapshot: reward)
       order.save
       redirect_to rewards_path, notice: 'Reward was successfully buying.'
     end
   end
-
+ 
   private
 
   def reward
     Reward.find(params[:reward])
   end
+
 end
