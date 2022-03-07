ffc41c7d3915869e15f13732596a9a6f9e3c0752
Michał Dobosz <doboszmichal@o2.pl>
2022-03-07 21:54:33 +0100

    add orders controller to admin

---
+++ app/controllers/admin/orders_controller.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+module Admin
+  class OrdersController < ApplicationController
+
+    def index
+      render :index, locals: { orders: Order.all }
+    end
+  end
+end
