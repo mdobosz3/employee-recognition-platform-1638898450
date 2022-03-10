bf62af5a81221d99d096740c283878107b4350fb
Michał Dobosz <doboszmichal@o2.pl>
2022-03-10 14:40:42 +0100

    add update method to admin_orders

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,9 +1,20 @@
 # frozen_string_literal: true
 
 module Admin
-  class OrdersController < ApplicationController
+  class OrdersController < AdminController
     def index
-      render :index, locals: { orders: Order.includes(:employee).all }
+      render :index, locals: { orders: Order.includes(:employee).all.order(:status)  }
+    end
+
+    def update
+      order.delivered!
+      redirect_to admin_orders_path, notice: 'Order was successfully delivered.'
+    end
+
+    private
+
+    def order
+      @order = Order.find(params[:id])
     end
   end
 end
