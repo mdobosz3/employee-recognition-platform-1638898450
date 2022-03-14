d2f11eba1cb36b573dae25986476ef0a29312b1c
Michał Dobosz <doboszmichal@o2.pl>
2022-03-15 00:39:44 +0100

    add a method for filtering in controller and model

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,7 +4,12 @@ class OrdersController < ApplicationController
   before_action :authenticate_employee!
 
   def index
-    render :index, locals: { orders: Order.where(employee: current_employee) }
+    #render :index, locals: { orders: Order.includes(params[:status]).where(employee: current_employee) }
+    if params[:status].present?
+      render :index, locals: { orders: Order.filter_by_status(params[:status]) }
+    else
+      render :index, locals: { orders: Order.all }
+    end
   end
 
   def create
