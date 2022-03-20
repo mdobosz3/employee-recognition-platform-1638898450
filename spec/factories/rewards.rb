47cde93165ceaa85dd59e306b01ac064931bcc9a
Michał Dobosz <doboszmichal@o2.pl>
2022-03-20 21:26:42 +0100

    change reward factories

--- spec/factories/rewards.rb
+++ spec/factories/rewards.rb
@@ -3,7 +3,7 @@
 FactoryBot.define do
   factory :reward do
     title { Faker::JapaneseMedia::DragonBall.character }
-    description { Faker::Movies::Hobbit.quote }
+    description { Faker::Creature::Animal.name }
     price { '1' }
   end
 end
