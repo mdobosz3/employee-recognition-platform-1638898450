def2ac76467c78a2bd5ef5529c0826fdbebdc5ce
Michał Dobosz <doboszmichal@o2.pl>
2022-03-20 22:50:10 +0100

    fix rubocop

--- app/mailers/order_delivery_mailer.rb
+++ app/mailers/order_delivery_mailer.rb
@@ -1,8 +1,7 @@
-class OrderDeliveryMailer < ApplicationMailer
+# frozen_string_literal: true
 
-    def delivery_email
-     # @order = params[:order]
-      mail(to: order.employee.email, subject: 'Your order has been delivered')
-     # mail(to: @order.employee.email, subject: '#{@order.reward_snapshot.title}Welcome to My Awesome Site')
-    end
+class OrderDeliveryMailer < ApplicationMailer
+  def delivery_email
+    mail(to: order.employee.email, subject: 'Your order has been delivered')
   end
+end
