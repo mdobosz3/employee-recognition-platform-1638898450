b28f8d828c90f387c6e58d8bd0dcb94879ff0796
Michał Dobosz <doboszmichal@o2.pl>
2022-03-22 08:33:11 +0100

    add order delivery mailer spec

---
+++ spec/mailers/previews/order_delivery_preview.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+# Preview all emails at http://localhost:3000/rails/mailers/order_delivery
+class OrderDeliveryPreview < ActionMailer::Preview]
+    def delivery_email
+        OrderDeliveryMailer.with(order: order).delivery_email
+    end
+end
