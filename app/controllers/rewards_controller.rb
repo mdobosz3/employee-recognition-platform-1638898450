127c68a7022f6588445b7328ceafb2fc64a6c1e4
Michał Dobosz <doboszmichal@o2.pl>
2022-04-05 11:14:14 +0200

    Add pagination to rewards controller

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -1,8 +1,17 @@
 # frozen_string_literal: true
 
 class RewardsController < ApplicationController
+  REWARDS_PER_PAGE = 10
+
   def index
-    render :index, locals: { rewards: Reward.all }
+   
+    @page = params.fetch(:page, 0).to_i
+    @number_of_page = (Reward.count / REWARDS_PER_PAGE).ceil
+     if params[:page].to_i <= @number_of_page
+    render :index, locals: { rewards: Reward.offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
+    else
+      redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_page+1}."
+    end
   end
 
   def show
