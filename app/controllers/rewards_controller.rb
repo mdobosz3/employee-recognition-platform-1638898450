bb14e4fa5a7981a6d8adadfbeea06ef43d9e06f3
Michał Dobosz <doboszmichal@o2.pl>
2022-04-05 11:26:45 +0200

    fix rubocop

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -4,13 +4,12 @@ class RewardsController < ApplicationController
   REWARDS_PER_PAGE = 10
 
   def index
-   
     @page = params.fetch(:page, 0).to_i
     @number_of_page = (Reward.count / REWARDS_PER_PAGE).ceil
-     if params[:page].to_i <= @number_of_page
-    render :index, locals: { rewards: Reward.offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
+    if params[:page].to_i <= @number_of_page
+      render :index, locals: { rewards: Reward.offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
     else
-      redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_page+1}."
+      redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_page + 1}."
     end
   end
 
--- spec/system/reward_spec.rb
+++ spec/system/reward_spec.rb
@@ -7,49 +7,51 @@ RSpec.describe 'Reward check', type: :system do
     driven_by(:rack_test)
   end
 
-  let(:employee) { create(:employee) }
-  let!(:reward) { create(:reward) }
+  context 'when the employee checks the rewards' do
+    let(:employee) { create(:employee) }
+    let!(:reward) { create(:reward) }
 
-  let!(:reward1) { create(:reward) }
-  let!(:reward2) { create(:reward) }
-  let!(:reward3) { create(:reward) }
-  let!(:reward4) { create(:reward) }
-  let!(:reward5) { create(:reward) }
-  let!(:reward6) { create(:reward) }
-  let!(:reward7) { create(:reward) }
-  let!(:reward8) { create(:reward) }
-  let!(:reward9) { create(:reward) }
-  let!(:reward10) { create(:reward, price: 3) }
+    let!(:reward1) { create(:reward) }
+    let!(:reward2) { create(:reward) }
+    let!(:reward3) { create(:reward) }
+    let!(:reward4) { create(:reward) }
+    let!(:reward5) { create(:reward) }
+    let!(:reward6) { create(:reward) }
+    let!(:reward7) { create(:reward) }
+    let!(:reward8) { create(:reward) }
+    let!(:reward9) { create(:reward) }
+    let!(:reward10) { create(:reward, price: 3) }
 
-  before do
-    sign_in employee
-    visit root_path
-    click_link 'Rewards'
-  end
-
-  it 'Reward index and show for employee' do
-    expect(page).to have_content reward.title
-    expect(page).not_to have_content reward.description
-    expect(page).to have_content reward.price
-
-    click_link 'Show', match: :first
-    expect(page).to have_content reward.title
-    expect(page).to have_content reward.description
-    expect(page).to have_content reward.price
-
-    click_link 'Back'
-    expect(page).to have_content reward.title
-    expect(page).not_to have_content reward.description
-    expect(page).to have_content reward.price
-  end
-
-  it 'Pagination check' do
-    expect(page).not_to have_content reward10.title
-    expect(page).not_to have_content reward10.price
-    within('[data-test-id="page_last"]') do
-      click_link '2'
+    before do
+      sign_in employee
+      visit root_path
+      click_link 'Rewards'
     end
-    expect(page).to have_content reward10.title
-    expect(page).to have_content reward10.price
+
+    it 'Reward index and show for employee' do
+      expect(page).to have_content reward.title
+      expect(page).not_to have_content reward.description
+      expect(page).to have_content reward.price
+
+      click_link 'Show', match: :first
+      expect(page).to have_content reward.title
+      expect(page).to have_content reward.description
+      expect(page).to have_content reward.price
+
+      click_link 'Back'
+      expect(page).to have_content reward.title
+      expect(page).not_to have_content reward.description
+      expect(page).to have_content reward.price
+    end
+
+    it 'Pagination check' do
+      expect(page).not_to have_content reward10.title
+      expect(page).not_to have_content reward10.price
+      within('[data-test-id="page_last"]') do
+        click_link '2'
+      end
+      expect(page).to have_content reward10.title
+      expect(page).to have_content reward10.price
+    end
   end
 end
