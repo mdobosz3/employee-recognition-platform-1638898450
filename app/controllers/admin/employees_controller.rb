f85650f40191a70dc90a82aeb8d5cf96b047d81a
Michał Dobosz <doboszmichal@o2.pl>
2022-04-09 12:40:15 +0200

    add transaction to employees controller

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -12,13 +12,18 @@ module Admin
 
     def update_kudos_for_all
       if add_kudos_param >= 1 && add_kudos_param <= 20
-        Employee.all.each do |employee|
-          employee.number_of_available_kudos += add_kudos_param
-          unless employee.save!
-            redirect_to edit_kudos_for_all_admin_employees_path,
-                        notice: 'There was an error. Please try again.'
+
+        begin
+          ActiveRecord::Base.transaction do
+            Employee.find_each do |employee|
+              employee.number_of_available_kudos += add_kudos_param
+              employee.save!
+            end
           end
+        rescue ActiveRecord::RecordNotSaved => e
+          raise "Something go wrong. Please try again. #{e.message}"
         end
+
         redirect_to admin_employees_path, notice: 'Number of available Kudos was successfully added to Employees.'
       else
         redirect_to edit_kudos_for_all_admin_employees_path, notice: 'Enter a number between 1 and 20.'
