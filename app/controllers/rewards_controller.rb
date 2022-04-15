6518138d0d118cffc9d00359ef78d1af33e90687
Michał Dobosz <doboszmichal@o2.pl>
2022-04-15 23:37:19 +0200

    update reward controller - category

--- app/controllers/rewards_controller.rb
+++ app/controllers/rewards_controller.rb
@@ -4,12 +4,22 @@ class RewardsController < ApplicationController
   REWARDS_PER_PAGE = 10
 
   def index
-    @page = params.fetch(:page, 0).to_i
-    @number_of_pages = (Reward.count / REWARDS_PER_PAGE).ceil
-    if params[:page].to_i <= @number_of_pages
-      render :index, locals: { rewards: Reward.offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
+    if params[:category_ids_arr].to_i > 0
+      @page = params.fetch(:page, 0).to_i
+      @number_of_pages = (Reward.filter_by_category(params[:category_ids_arr]).includes(:categories, :category_rewards).count / REWARDS_PER_PAGE).ceil
+      if params[:page].to_i <= @number_of_pages
+        render :index, locals: { rewards: Reward.filter_by_category(params[:category_ids_arr]).includes(:categories, :category_rewards).offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
+      else
+        redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_pages + 1}."
+      end
     else
-      redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_pages + 1}."
+      @page = params.fetch(:page, 0).to_i
+      @number_of_pages = (Reward.includes(:categories, :category_rewards).count / REWARDS_PER_PAGE).ceil
+      if params[:page].to_i <= @number_of_pages
+        render :index, locals: { rewards: Reward.includes(:categories, :category_rewards).offset(@page * REWARDS_PER_PAGE).limit(REWARDS_PER_PAGE) }
+      else
+        redirect_to rewards_path, notice: "Enter page in range 1 to #{@number_of_pages + 1}."
+      end
     end
   end
 
