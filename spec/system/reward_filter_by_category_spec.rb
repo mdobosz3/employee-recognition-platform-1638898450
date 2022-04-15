ad92497b8242e3542aa74fe794fc1933d69e090e
Michał Dobosz <doboszmichal@o2.pl>
2022-04-15 23:41:10 +0200

    add a spec for filter checking

---
+++ spec/system/reward_filter_by_category_spec.rb
@@ -0,0 +1,39 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'Reward filtering by category', type: :system do
+  before do
+    driven_by(:rack_test)
+  end
+
+  context 'when an employee filters reward' do
+    let!(:employee) { create(:employee) }
+    let!(:category_reward1) { create(:category_reward) }
+    let!(:category_reward2) { create(:category_reward) }
+
+    it 'when employee filters by category' do
+      sign_in employee
+
+      visit root_path
+      click_link 'Rewards'
+      expect(page).to have_content category_reward1.reward.title
+      expect(page).to have_content category_reward2.reward.title
+
+      select category_reward1.category.title, from: 'category_ids_arr'
+      click_button 'Filter'
+      expect(page).to have_content category_reward1.reward.title
+      expect(page).not_to have_content category_reward2.reward.title
+
+      select category_reward2.category.title, from: 'category_ids_arr'
+      click_button 'Filter'
+      expect(page).not_to have_content category_reward1.reward.title
+      expect(page).to have_content category_reward2.reward.title
+
+      select 'All Category', from: 'category_ids_arr'
+      click_button 'Filter'
+      expect(page).to have_content category_reward1.reward.title
+      expect(page).to have_content category_reward2.reward.title
+    end
+  end
+end
