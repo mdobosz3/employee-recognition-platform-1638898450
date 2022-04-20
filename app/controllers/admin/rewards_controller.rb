c0f3d07042cd0628ecc84f9afe32560988683722
Michał Dobosz <doboszmichal@o2.pl>
2022-04-20 11:40:24 +0200

    add photo to params - admin

--- app/controllers/admin/rewards_controller.rb
+++ app/controllers/admin/rewards_controller.rb
@@ -47,7 +47,7 @@ module Admin
     end
 
     def reward_params
-      params.require(:reward).permit(:title, :description, :price, category_ids: [])
+      params.require(:reward).permit(:title, :description, :price, :photo, category_ids: [])
     end
   end
 end
