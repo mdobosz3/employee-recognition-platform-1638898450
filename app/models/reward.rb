4ee389b68a757be78eafe3d0d566864dc23c6ccd
Michał Dobosz <doboszmichal@o2.pl>
2022-05-02 10:19:17 +0200

    add self.import method to reward model

--- app/models/reward.rb
+++ app/models/reward.rb
@@ -12,4 +12,23 @@ class Reward < ApplicationRecord
   has_many :categories, through: :category_rewards
 
   has_one_attached :photo
+
+  def self.import(file)
+    begin
+      ActiveRecord::Base.transaction do
+        CSV.foreach(file.path, headers: true) do |row|
+          if row[3] == nil
+            Reward.create_with(description: row[1], price: row[2]).find_or_create_by!(title: row[0])
+          else
+            title = row[3].split("-")
+            title[1] = -1 unless "#{title[0]}-" == "reward-" && row[0] == title[1]
+            reward = Reward.find_by!(title: title[1])
+            Reward.update(reward.id, row.to_h)
+          end
+        end
+      end
+    rescue ActiveRecord::RecordNotFound, CSV::MalformedCSVError
+    return false
+    end
+  end
 end
