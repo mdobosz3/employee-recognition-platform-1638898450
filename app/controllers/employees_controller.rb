d11dad0737acbf1a51602c41eb3b4bd392b7ebcc
Michał Dobosz <doboszmichal@o2.pl>
2022-05-09 18:29:16 +0200

    add employee controller

---
+++ app/controllers/employees_controller.rb
@@ -0,0 +1,41 @@
+# frozen_string_literal: true
+
+class EmployeesController < ApplicationController
+    before_action :authenticate_employee!
+    before_action :check_employee, only: %i[edit]
+  
+    def show
+      render :show, locals: { employee: current_employee }
+    end
+  
+    def edit
+      render :edit, locals: { employee: current_employee }
+    end
+  
+    def update
+    params[:employee].compact_blank! if params[:employee][:email].blank? && params[:employee][:password].blank?
+    if current_employee.first_name.nil? || current_employee.last_name.nil?
+      redirect_to edit_employee_path, notice: 'First name and last name cannot be empty.'
+    elsif current_employee.update(employee_params)
+      redirect_to root_path, notice: 'Your name was successfully added.'
+    else
+      render :edit
+    end
+
+end
+  
+    def check_employee
+      redirect_to root_path, notice: 'You are not authorized to edit this data.' unless current_employee.first_name.nil? && current_employee.last_name.nil?
+    end
+  
+    private
+  
+    def employee_params
+      params.require(:employee).permit(:first_name, :last_name, :email, :password)
+    end
+  
+#    def kudo
+#      @kudo = Kudo.find(params[:id])
+#    end
+  end
+  
