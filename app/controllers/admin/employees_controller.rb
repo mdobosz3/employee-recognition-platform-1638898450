df9b96a431872b2bfc70fbef8513e196688fd9c9
Michał Dobosz <doboszmichal@o2.pl>
2022-05-09 18:08:36 +0200

    update admin/employee controler RUD

--- app/controllers/admin/employees_controller.rb
+++ app/controllers/admin/employees_controller.rb
@@ -6,6 +6,28 @@ module Admin
       render :index, locals: { employees: Employee.includes(:orders).all }
     end
 
+    def edit
+      render :edit, locals: { employee: employee }
+    end
+
+    def update
+      params[:employee].compact_blank! if params[:employee][:password].blank?
+      
+      params[:employee][:first_name] = "test"
+      params[:employee][:last_name] = "test"
+      binding.pry
+      if employee.update(employee_params)
+        redirect_to admin_employees_path, notice: 'Employee was successfully updated.'
+      else
+        render :edit, locals: { employee: employee }
+      end
+    end
+
+    def destroy
+      employee.destroy
+      redirect_to admin_employees_path, notice: 'Employee was successfully destroyed.'
+    end
+
     def edit_kudos_for_all
       render :update_kudos_for_all, locals: { employees: Employee.all }
     end
@@ -31,6 +53,14 @@ module Admin
     end
 
     private
+
+    def employee
+      @employee = Employee.find(params[:id])
+    end
+
+    def employee_params
+      params.require(:employee).permit(:email, :password, :number_of_available_kudos)
+    end
 
     def add_kudos_param
       params[:add_kudos].to_i
