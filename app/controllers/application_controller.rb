e7959e4e987f1e4e848c23c96cb4caeea401a915
Michał Dobosz <doboszmichal@o2.pl>
2022-05-16 12:42:35 +0200

    add suggestion

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -20,6 +20,6 @@ class ApplicationController < ActionController::Base
   private
 
   def user_not_authorized
-    redirect_to kudos_path, notice: 'You can no longer edit or delete this kudo.'
+    redirect_to kudos_path, notice: 'Sorry, You are not authorized to do this.'
   end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -2,7 +2,7 @@
 
 class EmployeesController < ApplicationController
   before_action :authenticate_employee!
-  before_action :check_employee, only: %i[edit]
+  #before_action :check_employee, only: %i[edit]
 
   def show
     render :show, locals: { employee: current_employee }
@@ -13,8 +13,8 @@ class EmployeesController < ApplicationController
   end
 
   def update
-    params[:employee].compact_blank! if params[:employee][:email].blank? && params[:employee][:password].blank?
-    if params[:employee][:first_name].nil? || params[:employee][:last_name].nil?
+    authorize current_employee
+    if params[:employee][:first_name].blank? || params[:employee][:last_name].blank?
       redirect_to edit_employee_path, notice: 'First name and last name cannot be empty.'
     elsif current_employee.update(employee_params)
       redirect_to root_path, notice: 'Your name was successfully added.'
@@ -23,15 +23,9 @@ class EmployeesController < ApplicationController
     end
   end
 
-  def check_employee
-    return if current_employee.first_name.nil? && current_employee.last_name.nil?
-
-    redirect_to root_path, notice: 'You are not authorized to edit this data.'
-  end
-
   private
 
   def employee_params
-    params.require(:employee).permit(:first_name, :last_name, :email, :password)
+    params.require(:employee).permit(policy(current_employee).permitted_attributes)
   end
 end
